0df2c74d38034c7433c2f40b78b38dd689580a3c
s292463 <s292463@studenti.polito.it>
2021-12-04 13:14:30 +0100

    All strems correctly setted

--- screen_capture/Origine.cpp
+++ screen_capture/Origine.cpp
@@ -35,18 +35,18 @@ int main(int argc, const char* argv[]) {
 	avdevice_register_all();
 
 	// Option on recordig video
-	av_dict_set(&options, "framerate", "5", 0);
-	av_dict_set(&options, "offset_x", "20", 0);
-	av_dict_set(&options, "offset_y", "40", 0);
-	av_dict_set(&options, "video_size", "640x480", 0);
+	//av_dict_set(&options, "framerate", "5", 0);
+	//av_dict_set(&options, "offset_x", "20", 0);
+	//av_dict_set(&options, "offset_y", "40", 0);
+	//av_dict_set(&options, "video_size", "640x480", 0);
 
 	//auto *pAVInputFormat = av_find_input_format("dshow");
-	auto *pAVInputFormat = av_find_input_format("gdigrab");
+	//auto *pAVInputFormat = av_find_input_format("gdigrab");
 	
 	//int value = avformat_open_input(&formatContext_p, "video=screen-capture-recorder", pAVInputFormat, NULL);
-	int value = avformat_open_input(&formatContext_p, "desktop", pAVInputFormat, &options);
+	//int value = avformat_open_input(&formatContext_p, "desktop", pAVInputFormat, &options);
 
-	//int value = avformat_open_input(&formatContext_p, fileName, nullptr, nullptr);
+	int value = avformat_open_input(&formatContext_p, fileName, nullptr, nullptr);
 
 	if (value != 0)
 	{
@@ -56,6 +56,30 @@ int main(int argc, const char* argv[]) {
 
 
 	std::cout << fileName << std::endl << "Format " << formatContext_p->iformat->long_name << "\nDuration: " << formatContext_p->duration * 0.000001 << " sec" << std::endl;
+	std::cout << "Number of streams: "<<formatContext_p->nb_streams<<std::endl;
+
+	for (int i = 0; i < formatContext_p->nb_streams; i++)
+	{
+		// Estraiamo i parametri del codec dello stream dati (audio/video)
+		AVCodecParameters *videoCodecPar = formatContext_p->streams[i]->codecpar;
+		// Tra i parametri trovati usiamo l' id del codec per trovare il decoder che ci serve
+		const AVCodec *videoCodec = avcodec_find_decoder(videoCodecPar->codec_id);
+
+		// codec_type definisce il tipo di stream su cui viene applicato il codec
+		// codec_id definisce il tipo di codec in cui e'codificato lo stream 
+		if (videoCodecPar->codec_type == AVMEDIA_TYPE_VIDEO)
+		{
+			std::cout<<"Video Codec: resolution "<<videoCodecPar->width<<"x" << videoCodecPar->height<<std::endl;
+		}
+		else if (videoCodecPar->codec_type == AVMEDIA_TYPE_AUDIO)
+		{
+			std::cout << "Audio Codec: "<< videoCodecPar->channels << " channels, Sample Rate: " << videoCodecPar->sample_rate<< std::endl;
+		}
+
+		// General info about codecs
+		std::cout<<"\tCodec "<<videoCodec->long_name <<", ID: "<< videoCodec->id << ", bit_rate: "<<videoCodecPar->bit_rate << std::endl;;
+	}
+
 
 	return 0;
 }
