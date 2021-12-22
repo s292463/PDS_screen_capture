f5cece7a1e0c625c8206dbaa94a15a794ec3a471
Elia Fontana s290188 <s290188@studenti.polito.it>
2021-12-22 18:14:31 +0100

    working on audio recording

---
+++ screen_capture/AudioRecorder.h
@@ -0,0 +1,60 @@
+#ifndef AUDIORECORDER_AUDIORECORDER_H
+#define AUDIORECORDER_AUDIORECORDER_H
+
+//#ifdef WINDOWS
+//#include "ListAVDevices.h"
+//#endif
+
+#include "ffmpeg.h"
+#include <string>
+#include <cstdint>
+#include <iostream>
+
+//#ifdef __linux__
+#include <atomic>
+#include <thread>
+//#endif
+
+using std::string;
+
+class AudioRecorder {
+
+private:
+    string outfile;
+    string deviceName;
+    string failReason;
+
+    AVFormatContext *audioInFormatCtx;
+    AVStream        *audioInStream;
+    AVCodecContext  *audioInCodecCtx;
+
+    SwrContext      *audioConverter;
+    AVAudioFifo     *audioFifo;
+
+    AVFormatContext *audioOutFormatCtx;
+    AVStream        *audioOutStream;
+    AVCodecContext  *audioOutCodecCtx;
+
+    std::atomic_bool     isRun;
+    std::thread         *audioThread;
+
+    void StartEncode();
+
+
+
+public:
+
+    AudioRecorder(string filepath, string device)
+            :outfile(filepath),deviceName(device),failReason(""),isRun(false){}
+
+    void Open();
+    void Start();
+    void Stop();
+
+    ~AudioRecorder() {
+        Stop();
+    }
+
+    std::string GetLastError() { return failReason; }
+};
+#endif //AUDIORECORDER_AUDIORECORDER_H
--- screen_capture/main.cpp
+++ screen_capture/main.cpp
@@ -2,10 +2,11 @@
 #include <iostream>
 
 
-int main() {
+int ciccio() {
 
 	Resolution res = { "1920", "1080" };
-	std::string outputFileName = "C:/Users/chrees/Desktop/output.mp4";
+	std::string outputFileName = "C:/Users/elia_/OneDrive/Desktop/output.mp4";
+	//std::string outputFileName = "C:/Users/chrees/Desktop/output.mp4";
 	std::string offset_x = "0";
 	std::string offset_y = "0";
 
@@ -17,4 +18,37 @@ int main() {
 	value = capturer.startCapturing(100);
 
 	return 0;
+}
+
+
+#include "AudioRecorder.h"
+#include <string>
+
+using namespace std;
+
+int main() {
+    puts("==== Audio Recorder ====");
+    avdevice_register_all();
+
+    AudioRecorder recorder{ "C:/Users/elia_/OneDrive/Desktop/testAudio.aac","" };
+    try {
+        recorder.Open();
+        recorder.Start();
+
+        //record 10 seconds.
+        std::this_thread::sleep_for(10s);
+
+        recorder.Stop();
+        string reason = recorder.GetLastError();
+        if (!reason.empty()) {
+            throw std::runtime_error(reason);
+        }
+    }
+    catch (std::exception& e) {
+        fprintf(stderr, "[ERROR] %s\n", e.what());
+        exit(-1);
+    }
+
+    puts("END");
+    return 0;
 }
